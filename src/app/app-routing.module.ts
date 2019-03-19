067ea6b45da9ded4c19937b2d9c78a3bdcffeaf8
cjropero <cjavieramaya@gmail.com>
2019-03-18 21:38:15 -0500

    generacion de vista (registrar)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'registrar', loadChildren: './auth/registrar/registrar.module#RegistrarPageModule' },
 ];
 
 @NgModule({
