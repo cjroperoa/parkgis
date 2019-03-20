30f691bc5c256d02ef291b3f32b6cace6e557ef8
cjropero <cjavieramaya@gmail.com>
2019-03-19 21:47:58 -0500

    creacion de metodos de solicitud de permiso de acceso GPS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+/* Complementos 
+cordova-plugin-android-permissions
+cordova-plugin-request-location-precision
+cordova-plugin-request-location-precision */
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    //Carga de complementos
+    AndroidPermissions,
+    Geolocation,
+    LocationAccuracy,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
