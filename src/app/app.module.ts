41b100ddff650cb8680b5bafe9081e0c90076347
cjropero <cjavieramaya@gmail.com>
2019-04-19 23:03:38 -0500

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,29 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-/*  PLUGINS
-cordova-plugin-android-permissions
-cordova-plugin-request-location-precision
-cordova-plugin-request-location-precision */
-import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AgmCoreModule.forRoot()
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    //Carga de complementos
-    AndroidPermissions,
-    Geolocation,
-    LocationAccuracy,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
